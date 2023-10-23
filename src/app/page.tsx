dc4a2ee167e40d8555b7a746d49067183f559e8e
mickaias <kawamickaias@gmail.com>
2023-10-23 13:11:58 -0400

    feat: criando projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      hello next 13
+    </div>
+  )
+}
